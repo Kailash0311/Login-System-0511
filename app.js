b1bebc3cbe54cf25716377e2783d7ae569158836
Kailash Bhalaki <kailashbhalaki@gmail.com>
2018-02-13 23:23:00 +0530

    mongodb setup and bug fixes

--- app.js
+++ app.js
@@ -4,15 +4,15 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
-
+var app = express();
 var index = require('./routes/index');
 var users = require('./routes/users');
+//var findOrCreate=require('mongoose-findorcreate');
 
 //mongoose database connection
 
 var mongoose=require('mongoose');
-var url='mongodb://localhost:27017/user'
+var url='mongodb://localhost:27017/1user'
  var db
 
  
@@ -25,24 +25,25 @@ if(err)
   }
 else
   {
+    db=database;
     console.log("Connected succesfully to the server...")
   }
 }
 
 )
-
-//schema for mongoose
-
-/*mongoose.model('User',{
-  *username:String,
-  email: String,
-  gender: String,
-  facebookId:Number
-});*/
-
+var passport=require('passport'), 
+FacebookStrategy=require('passport-facebook').Strategy;
 //passport
+app.use(passport.initialize());
+app.use(passport.session());
 
-var passport=require('passport'), FacebookStrategy=require('passport-facebook').Strategy;
+
+passport.serializeUser(function(user, done) {
+  done(null, user);
+});
+passport.deserializeUser(function(obj, done) {
+  done(null, obj);
+})
 
 var Schema = mongoose.Schema;
 var UserSchema = new Schema(
@@ -51,58 +52,94 @@ var UserSchema = new Schema(
   username:String,
   email: String,
   gender: String,
+  provider:String
 
 });
 
 var User = mongoose.model('User', UserSchema);
 
+
+app.get('/auth/facebook', passport.authenticate('facebook', { scope: ['email']}));
+
+app.get('/auth/facebook/callback', 
+  passport.authenticate('facebook', { successRedirect:'/users',failureRedirect: '/'}),
+function(req,res){
+  res.redirect('/');
+}
+);
+
+ 
+
 passport.use(new FacebookStrategy({
 clientID:'1997237017267937',
-clientSecret: 'xxxxxxxxxxxxxx',
-callbackURL:"http://localhost:3000/auth/facebook/callback"
-
+clientSecret: '961f12f5ac42d1defe1aaf602f506213',
+callbackURL:"http://localhost:3000/auth/facebook/callback",
+profileFields: ['id', 'displayName', 'photos', 'email'],
+enableProof: true
 },
 function(accesToken,refreshToken,profile,done){
   User.findOne({facebookId:profile.id} , function(err,user){
     if(err){
       return done(err);
     }
-    if(!user){
-            
-            user : new User({
-            username:profile.displayName,
-            email:profile.email,
-            
-      },
-      console.log(profile.displayName));
-      return done(null,user);
-     /* user.save(function(err)
+    if(!user)
+    {
+      //adding the new user's data
+            user = new User
+            ({
+              provider: 'facebook',
+              facebook: profile._json,
+              username:profile.displayName,
+              email:profile.email,
+              facebookId:profile.id
+          
+            }),
+      //saving and adding user into the database
+
+      user.save().then(function(user)
       {
-          if(err) console.log(err);
-          return done(err,user);
+       
+        //if(err){console.log("not inserted")}
+        if(user){
+          console.log(user.username + "inserted into the database collection"+user.email)
+        };
 
-      })*/
-    }
+      });
+      
+
+        console.log(profile.displayName+" has logged in with e-mail id : "+ profile.emails)
+        //name is made as global so that it can be accessible from other files and can be rendered to the jade template :)
+          global.name=profile.displayName;
+          return done(null,user);
+    } 
     else{
+      
+      //updating and overwriting the user's data , if changed 
+      user = new User
+            ({
+              provider: 'facebook',
+              facebook: profile._json,
+              username:profile.displayName,
+              email:profile.email,
+              facebookId:profile.id
+          
+            })
+          global.name=profile.displayName;
+      console.log("welcome "+ user.username+profile.emails);
       return done(null,user);
-      //console.log("welcome "+ profile.displayName);
     }
-    
-
   })
-}
-));
+}))
 
-var app = express();
 
-app.get('/auth/facebook', passport.authenticate('facebook'));
+app.get('/logout', function(req, res){
+  console.log("hello")
+  req.logOut();
+  res.redirect('/');
+});
 
-app.get('/auth/facebook/callback', 
-  passport.authenticate('facebook', { failureRedirect: '/' }),
-  function(req, res) {
-    console.log("userss...")
-    res.redirect('/users');
-  });
+
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -137,4 +174,4 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
-module.exports = app;
+module.exports = app 
